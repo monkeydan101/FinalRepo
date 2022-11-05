cee1c4a24f56214ee516e949ce37beed6465b23c
monkeydan101 <117491939+monkeydan101@users.noreply.github.com>
2022-11-05 12:25:17 -0600

    newnew

--- gamejammin2022/Assets/player_movement.cs
+++ gamejammin2022/Assets/player_movement.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public int mov_speed; 
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         Debug.Log("Hello bruh");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("space"))
+        float direcX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(7f * direcX, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 10, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector3(rb.velocity.x, 20, 0);
         } 
+
+
     }
 }
