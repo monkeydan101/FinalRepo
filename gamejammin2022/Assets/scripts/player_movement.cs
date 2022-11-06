a55a19e5b690811cb0ccda1117ce22ced946160c
monkeydan101 <117491939+monkeydan101@users.noreply.github.com>
2022-11-06 11:50:50 -0700

    bruh

--- gamejammin2022/Assets/scripts/player_movement.cs
+++ gamejammin2022/Assets/scripts/player_movement.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class player_movement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private Animator anim;
     private SpriteRenderer sprite;
-    
+
     public float mov_speed = 7f;
     private float direcX = 0;
     public float jumpForce = 20f;
@@ -17,9 +16,8 @@ public class player_movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
-        
+
     }
 
     // Update is called once per frame
@@ -32,7 +30,7 @@ public class player_movement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector3(rb.velocity.x, jumpForce, 0);
-        } 
+        }
 
         UpdateAnim();
 
@@ -40,16 +38,17 @@ public class player_movement : MonoBehaviour
 
     private void UpdateAnim()
     {
- 
         if (direcX > 0f)
-        { 
+        {
             sprite.flipX = false;
         }
         else if (direcX < 0f)
         {
+
             sprite.flipX = true;
         }
 
-       
+
     }
+
 }
