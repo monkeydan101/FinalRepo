d9165c6277a430e3ed43010b9fe962b84a88cbaf
monkeydan101 <117491939+monkeydan101@users.noreply.github.com>
2022-11-06 15:31:25 -0700

    BIG CHANGES

--- gamejammin2022/Assets/scripts/player_movement.cs
+++ gamejammin2022/Assets/scripts/player_movement.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class player_movement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private BoxCollider2D coll;
 
     public float mov_speed = 7f;
     private float direcX = 0;
     public float jumpForce = 20f;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     public float air_amount;
 
@@ -21,7 +25,8 @@ public class player_movement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         air_amount = 10f;
-
+        coll = GetComponent<BoxCollider2D>();
+        
 
     }
 
@@ -32,9 +37,14 @@ public class player_movement : MonoBehaviour
 
         rb.velocity = new Vector2(mov_speed * direcX * air_amount/3, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded())
         {
             GetComponent<Rigidbody2D>().velocity = new Vector3(rb.velocity.x, jumpForce - (air_amount / 5), 0);
+        }
+
+        else if (Input.GetKeyDown("r"))
+        {
+            SceneChange();
         }
 
         UpdateAnim();
@@ -66,6 +76,11 @@ public class player_movement : MonoBehaviour
         }
     }
 
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
     private void UpdateAir()
     {
 
@@ -73,7 +88,25 @@ public class player_movement : MonoBehaviour
 
 
 
-        air_amount -= (Time.deltaTime)/2;
+        air_amount -= (Time.deltaTime - Time.deltaTime/6);
+
+        if(air_amount <= 0)
+        {
+            death();
+        }
+    }
+
+    private void death()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        transform.localRotation = Quaternion.Euler(0, 90, 0);
+        Invoke("SceneChange", 3);
+
+    }
+
+    private void SceneChange()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
